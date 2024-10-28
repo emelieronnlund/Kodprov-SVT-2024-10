0b628ed66287d5e493f190a23bad48381cbafc33
Emelie Rönnlund <emelieronnlund@gmail.com>
2024-10-28 23:23:19 +0100

    Initial commit

---
+++ src/main/kotlin/com/galacticempire/starships/StarshipsApplication.kt
@@ -0,0 +1,11 @@
+package com.galacticempire.starships
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StarshipsApplication
+
+fun main(args: Array<String>) {
+	runApplication<StarshipsApplication>(*args)
+}
